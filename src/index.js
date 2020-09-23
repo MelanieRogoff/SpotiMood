fb1927e2ca65e81017332c742ba883285d7df30a
Melanie <mgrogoff@gmail.com>
2020-09-23 02:02:11 -0700

    Resolved merge conflict, changed filenames to camel-casing, made the happy emoticon pink in order to differentiate from the blue emoticon

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import {HomeScreen} from './screens';
+import { HomeScreen } from './screens/homeScreen'
 
 ReactDOM.render(
   <React.StrictMode>
--- src/screens/homeScreen/HomeScreen.js
+++ src/screens/homeScreen/HomeScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './homeScreen.scss';
 import { Smiley } from '../../components/smiley/smiley'; //we can group the imports because we did the named export and exported all of the components from the index.js , which makes our imports cleaner 
 import { Box, Typography } from '@material-ui/core';
-import { SpotifyBox } from '../../components/spotifyBox/SpotifyBox';
+import { SpotifyBox } from '../../components/spotifyBox/spotifyBox';
 
 /**
  * HomeScreen -- this replaces App.js
@@ -11,15 +11,12 @@ import { SpotifyBox } from '../../components/spotifyBox/SpotifyBox';
 export function HomeScreen() {
   return (
     // Use Material-UI instead of DOM primitives (ie: div, etc)
-    // look into setting up Material-UI themes so our components share a global theme
     <Box className="homeScreen">
       <Typography variant='h4' className={'headers'}>SpotiMood</Typography>
       <Typography variant='h6' className={'headers'}>
         Pick a mood below in order to view your customized Spotify playlist!
       </Typography>
-        {/* 
-          for any components that don't have children (aka other components in between them) 
-          we can just call the component like this. there's 0 performance boost, but its less things to read on the screen
+        {/* for any components that don't have children (aka other components in between them) we can just call the component like this. 0 performance boost, but its less things to read on the screen
         */}
       <Smiley/>
       <SpotifyBox/>
